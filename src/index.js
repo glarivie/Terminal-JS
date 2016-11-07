76eba4f7a4d9e0f16182152fe8aed3c00b6d0429
Guillaume <glarivie@student.42.fr>
2016-11-07 18:30:15 +0100

    refactor

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import 'minireset.css';
+import './index.css';
+
 import App from './App';
 
-import './index.css';
 
 render(<App />, document.getElementById('root'));
